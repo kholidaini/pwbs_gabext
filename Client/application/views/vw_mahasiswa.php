eb2914ea609ea45e022f26f4c214b36d26e2002a
kholidaini <kholidainii@gmail.com>
2022-11-15 18:05:17 +0700

    15 nov nonfix

--- Client/application/views/vw_mahasiswa.php
+++ Client/application/views/vw_mahasiswa.php
@@ -48,7 +48,26 @@
 
         ?>
         <tr>
-        <td style="text-align: center;"><i class="fa-solid fa-trash"></i></td>
+            <td style="text-align: center;">
+
+            <nav class="area-aksi">
+                <!-- tombol ubah -->
+                <button class="btn-ubah" id="btn_ubah" title="Ubah Data Mahasiswa" 
+                onclick="return gotoUpdate('<?php echo $record->npm_mhs;?>')">
+                    <i class="fa-solid fa-pen">
+                        
+                    </i>
+                </button>
+                <!-- tombol hapus -->
+                <button class="btn-hapus" id="btn_hapus" title="Hapus Data Mahasiswa"
+                onclick="return gotoDelete('<?php echo $record->npm_mhs;?>')">
+                    <i class="fa-solid fa-trash">
+
+                    </i>
+                </button>
+            </nav>
+
+            </td>
             <td style="text-align: center;"><?php echo $no;?></td>
             <td style="text-align: center;"><?php echo $record->npm_mhs;?></td>
             <td style="text-align: left;"><?php echo $record->nama_mhs;?></td>
@@ -89,7 +108,7 @@
 
             // alihkan ke halaman (controller) tambah mahasiswa
             
-            // location.href='<?php echo site_url("Mahasiswa/addMahasiswa");?>
+            location.href='<?php echo site_url("Mahasiswa/addMahasiswa");?>';
 
             // manipulasi html
             // this.value="Entry Data"
@@ -101,10 +120,15 @@
             // this.style.fontSize="40px"
 
             // manipulasi dengan mengambil class
-            this.className= 'btn-+secondary'
+            this.className= 'btn-secondary'
 
-        })
+        });
         
+        // buat fungsi ubah data`
+        function gotoUpdate(npm){
+            // alihkan ke halaman ubah data
+            location.href='<?php echo site_url("Mahasiswa/updateMahasiswa");?>'+'/'+npm;
+        }
 
         
     </script>
